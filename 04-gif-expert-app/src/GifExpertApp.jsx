3143b2de1fe1a5337636f1ac2c8b825239e284d3
Bricia <briciacdiaz@gmail.com>
2024-05-09 18:00:18 -0300

    app gifs

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
 export const GifExpertApp = () => {
-    
+
+  const [categories, setCategories] = useState(['barbie']);
+
+  const onAddCategory = (newCategory) => {
+    if(categories.includes(newCategory)) return; //valida si la categoria ya existe
+    //console.log(newCategory);
+    setCategories([...categories, newCategory]);
+  }
+
   return (
     <>
+
         <h1>GifExpertApp</h1>
+
+        <AddCategory onNewCategory={event => onAddCategory(event)}/>
+
+          {categories.map((category) => 
+            (
+              <GifGrid key={category} category={category}/>
+          ))}
+
     </>
 
   )
 }
+
