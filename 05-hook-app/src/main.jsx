4737cf562e41af83bda2839fdfe788f4791828c8
Bricia <briciacdiaz@gmail.com>
2024-06-02 02:02:24 -0300

    cambios

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from 'react-router-dom';
 //import {HooksApp} from './HooksApp';
 //import { CounterApp } from './01-useState/CounterApp.jsx';
 //import { CounterWCoustomHook } from './01-useState/CounterWCoustomHook.jsx';
@@ -11,15 +12,17 @@ import ReactDOM from 'react-dom/client'
 //import { Memorize } from './06-memos/Memorize.jsx';
 //import { MemoHook } from './06-memos/MemoHook.jsx';
 //import { CallbackHook } from './06-memos/CallbackHook.jsx';
-import { Padre } from './07-tarea-memo/Padre.jsx';
+//import { Padre } from './07-tarea-memo/Padre.jsx';
+import { MainApp } from './09-useContext/MainApp.jsx';
 
 
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-    //<FormWCustomHook/>
-    <Padre/>
-  //</React.StrictMode>,
+  <BrowserRouter>
+  {/*<React.StrictMode>*/}
+    <MainApp/>
+  {/*</React.StrictMode>*/}
+  </BrowserRouter>
 )
